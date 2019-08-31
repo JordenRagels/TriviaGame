ae5720bd7778edf0a3240434a796021694d7ba54
Jorden Ragels <jordenragels3@gmail.com>
2019-08-31 13:31:09 -0700

    made an object for the questions and updated the html

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+
+
+// Make an object for values
+
+var questionsArray = [
+    { question: "How old was Snow White?", choices: [20, 14, 16], correctAnswer: 14 },
+    { question: "Which Princess has dimples?", choices: [Cinderella, Jasmine, Tiana], correctAnswer: Tiana },
+    { question: "Which Princesses are left handed?", choices: [Mulan & Tiana, Pocahontas & Aurora, Merida & Mulan], correctAnswer: Mulan & Tiana },
+    { question: "Which is the quietest Princess?", choices: [Cinderella, Snow White, Aurora], correctAnswer: Aurora },
+    { question: "Which Princess was the only one with brothers?", choices: [Ariel, Belle, Merida] correctAnswer: Merida},
+    { question: "Which Princess was Walt's favorite?", choices: [Cinderella, Snow White, Aurora] correctAnswer: Cinderella},
+    { question: "Pocahontas was the only Princess based off of a real person?", choices: [True, False], correctAnswer: True},
+    { question: "Which accent does Aurora have?", choices: [Dutch, English, French], correctAnswer: English},
+    { question: "Which was the last Disney Princess made with Walt?", choices: [Snow White, Aurora, Cinderella], correctAnswer: Aurora},
+    { question: "How many engineers and artists did it take to make Merida's hair?", choices: [4, 6, 10], correctAnswer: 6}
+]
+
+// Start button 
+
+//Timer 
+
+// put the questions on the screen 
+
+// for each question 
+
+
+
